c87fd93b63a1d0a4680a909979341a01252ff0e1
menneli <jariktsum2007@gmail.com>
2025-10-02 14:30:24 +0300

    Fixed errors

--- homework00/test_hello_world.py
+++ homework00/test_hello_world.py
@@ -1,10 +1,12 @@
+"""Works with text"""
 import unittest
 
 import hello_world
 
 
 class HelloTestCase(unittest.TestCase):
+    """Class to work with a definition"""
     def test_hello(self):
+        """Works with messages"""
         m = "message"
         self.assertEqual(m, hello_world.text())
-        
