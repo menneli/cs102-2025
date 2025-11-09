9e82db25f8330c97d0cad7bbe5696a0fa28fbb0b
menneli <131284866+menneli@users.noreply.github.com>
2025-11-09 21:32:57 +0300

    Исправлены ошибки

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -204,7 +204,6 @@ def check_solution(solution: tp.List[tp.List[str]]) -> bool:
     return True
 
 
-
 def generate_sudoku(N: int) -> tp.List[tp.List[str]]:
     """Генерация судоку заполненного на N элементов
     >>> grid = generate_sudoku(40)
